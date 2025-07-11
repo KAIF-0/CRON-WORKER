4e2ed259c27bb03fdaaa8356b5e5934e56abde6f
KAIF KHAN <kaif.2116401523@std.ggsipu.ac.in>
2025-07-11 07:01:49 +0000

    added: cron worker added

--- src/main.js
+++ src/main.js
@@ -1,35 +1,56 @@
 import { Client, Users } from 'node-appwrite';
+import dotenv from 'dotenv';
+import axios from 'axios';
+dotenv.config();
 
-// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers['x-appwrite-key'] ?? '');
   const users = new Users(client);
 
+  const URL = JSON.parse(process.env.URL);
+  let number = Object.keys(URL).length;
+
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
+    while (number) {
+      const url = URL[`${number}`];
+      log(`Executing cron job for URL: ${url}`);
+      await axios.get(url)
+        .then((response) => {
+          log(`Cron job for ${url} executed successfully:`, response.data);
+        })
+        .catch((err) => {
+          error(`Error executing cron job for ${url}:`, err.message);
+        });
+      number--;
+    }
+  } catch (err) {
     error("Could not list users: " + err.message);
   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
   return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
+    succes: true,
+    message: `${number} cron jobs successfully executed!`,
   });
 };
+
+
+
+//for testing
+// (async () => {
+//   const { default: main } = await import('./main.js');
+//   const req = {
+//     headers: {
+//       'x-appwrite-key': process.env.APPWRITE_FUNCTION_API_KEY,
+//     },
+//   };
+//   const res = {
+//     json: (data) => console.log('Response:', data),
+//   };
+//   const log = console.log;
+//   const error = console.error;
+
+//   await main({ req, res, log, error });
+// })();
